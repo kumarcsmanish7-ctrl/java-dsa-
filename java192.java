6e72555d2750e6c2bb46fdeda14697a3b09e07e3
kumarcsmanish7-ctrl <kumarcsmanish7@gmail.com>
2026-05-03 13:49:34 +0530

    update

---
+++ java192.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class java192 {//indian coins 
+    public static void main(String args[]){
+        Integer coins[] = {1,2,5,10,20,50,100,500,2000};
+        Arrays.sort(coins,Comparator.reverseOrder());
+        int countcoins =0;
+        int amount = 590;
+        ArrayList<Integer> ans  = new ArrayList<>();
+        for(int i =0;i<coins.length;i++){
+            if(coins[i]<=amount){
+                while(coins[i]<=amount){
+                    countcoins++;
+                    amount-=coins[i];
+
+                }
+            }
+        }
+        System.out.println("total min coins used:"+countcoins);
+        for(int i=0;i<ans.size();i++){
+            System.out.print(ans.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
