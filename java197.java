fb16ae46a886585c04564e58230bee269bcedbe8
kumarcsmanish7-ctrl <kumarcsmanish7@gmail.com>
2026-05-11 23:20:50 +0530

    update

--- java197.java
+++ java197.java
@@ -251,6 +251,22 @@ public class java197 {
         }
         return max+1;
     }
+
+
+    //O(n)
+    public static int transform(Node root){
+        if(root==null){
+            return 0;
+        }
+        int leftchild = transform(root.left);
+        int rightchild = transform(root.right);
+        int data = root.data;
+        int newleft = root.left == null ? 0: root.left.data;
+        int newright = root.right == null? 0: root.right.data;
+        root.data = leftchild + newleft + newright +rightchild;
+        return data;
+    }
+
     public static void main(String args[]){
         /*
         1
