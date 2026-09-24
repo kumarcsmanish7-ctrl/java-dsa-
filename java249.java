080beda74f1cd363fb2ddf22fca082f7aa399a2f
kumarcsmanish7-ctrl <kumarcsmanish7@gmail.com>
2026-09-24 18:10:26 +0530

    updated project

--- java249.java
+++ java249.java
@@ -1,16 +1,26 @@
+import java.util.*;
 public class java249 {
-    public static int countWays(int n){
+    public static int countWays(int n,int[] ways){// memorization 
         if(n ==0){
             return 1;
         }
         if(n<0){
             return 0;
         }
-        return countWays(n-1)+countWays(n-2);
+        if(ways[n]!=-1){
+            // already calculated 
+            return ways[n];
+
+        }
+        ways[n]=  countWays(n-1,ways)+countWays(n-2,ways);
+        return ways[n];
     }
     public static void main(String args[]){
         int n = 5; // n = 3 ->3 & n=4 -> 5 => 8 
-        System.out.println(countWays(n));
+ 
+        int ways[] = new int[n+1];
+        Arrays.fill(ways,-1);
+        System.out.println(countWays(n,ways));
 
     }
 }
